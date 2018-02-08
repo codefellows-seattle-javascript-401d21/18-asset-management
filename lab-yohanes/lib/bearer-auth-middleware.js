9d3b7e6ce83dc2df421045ab51cdf6598f2ff925
Yohanes Demissie <Yohanesdemissie@gmail.com>
2018-02-07 17:35:57 -0800

    all files/tests/code scaffolded and passing. Time for AWS

--- lab-yohanes/lib/bearer-auth-middleware.js
+++ lab-yohanes/lib/bearer-auth-middleware.js
@@ -1,23 +1,42 @@
 'use strict';
 
 const errorHandler = require('./error-handler');
+const Auth = require('../model/auth');
+const jsonWebToken = require('jsonwebtoken');
 
-module.exports = function (req, res, next) {
-  let authHeaders = req.headers.authorization;
-  if (!authHeaders)
-    return errorHandler(new Error('Authorization failed. Headers do not match requirements.'), res);
+//  making the name capitalized since it's a constant string
+const ERROR_MESSAGE = 'Authorization Failed';
 
-  let base64 = authHeaders.split('Basic ')[1];//base 64 is a decoder
-  if (!base64)
-    return errorHandler(new Error('Authorization failed. Username and Password required.'), res);
+module.exports = function (request, response, next) {
 
-  let [username, password] = Buffer.from(base64, 'base64').toString().split(':');
-  req.auth = { username, password };
+  let authHeader = request.headers.authorization;
 
-  if (!req.auth.username)
-    return errorHandler(new Error('Authorization failed. Username required.'), res);
-  if (!req.auth.password)
-    return errorHandler(new Error('Authorization failed. Password required.'), res);
+  if (!authHeader)
+    return errorHandler(new Error(ERROR_MESSAGE), response);
 
-  next();
+  let token = authHeader.split('Bearer ')[1];
+
+  if (!token)
+    return errorHandler(new Error(ERROR_MESSAGE), response);
+
+  //  -at this point, we have a TOKEN
+  //  -verify === decrypt
+  return jsonWebToken.verify(token, process.env.APP_SECRET, (error, decodedValue) => {
+    if (error) {//
+      error.message = ERROR_MESSAGE;
+      return errorHandler(error, response);
+    }
+    //  -at this point, we have the tokenSeed / compareHash i.e. {token: mario}
+
+    return Auth.findOne({ compareHash: decodedValue.token })
+      .then(user => {
+        if (!user)
+          return errorHandler(new Error(ERROR_MESSAGE), response);
+        //  -we are mutating the request(hash) with a user
+        //  -at this point, we are logged in
+        request.user = user;
+        next();
+      })
+      .catch(error => errorHandler(error, response));
+  });
 };
